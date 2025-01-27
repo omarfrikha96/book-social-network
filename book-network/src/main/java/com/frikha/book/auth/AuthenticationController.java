2fd1f46110280a5a8f09d03d07a44e596c5a64ac
omarfrikha96 <omar.frikha96@gmail.com>
2025-01-27 15:45:31 +0100

    add authenticate api

--- book-network/src/main/java/com/frikha/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/frikha/book/auth/AuthenticationController.java
@@ -26,5 +26,10 @@ public class AuthenticationController {
         service.register(request);
         return ResponseEntity.accepted().build();
     }
-
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
 }
--- book-network/src/main/java/com/frikha/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/frikha/book/auth/AuthenticationService.java
@@ -3,6 +3,7 @@ package com.frikha.book.auth;
 import com.frikha.book.email.EmailService;
 import com.frikha.book.email.EmailTemplateName;
 import com.frikha.book.role.RoleRepository;
+import com.frikha.book.security.JwtService;
 import com.frikha.book.user.Token;
 import com.frikha.book.user.TokenRepository;
 import com.frikha.book.user.User;
@@ -10,11 +11,14 @@ import com.frikha.book.user.UserRepository;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -25,6 +29,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
     private final EmailService emailService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Value("${application.mailing.frontend.activation-url}")
     private String activationUrl;
@@ -81,4 +87,21 @@ public class AuthenticationService {
         return codeBuilder.toString();
     }
 
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        var auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var claims = new HashMap<String, Object>();
+        var user = ((User) auth.getPrincipal());
+        claims.put("fullName", user.getFullName());
+
+        var jwtToken = jwtService.generateToken(claims, (User) auth.getPrincipal());
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
 }
