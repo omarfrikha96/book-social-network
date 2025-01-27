9c8f57b63f07850d4252cb0ac1df3194898620f7
omarfrikha96 <omar.frikha96@gmail.com>
2025-01-27 22:14:22 +0100

    Add update Archived Status

--- book-network/src/main/java/com/frikha/book/book/BookController.java
+++ book-network/src/main/java/com/frikha/book/book/BookController.java
@@ -73,5 +73,13 @@ public class BookController {
     public ResponseEntity<Integer> updateShareableStatus(@PathVariable("book-id") Integer bookId, Authentication connectedUser) {
         return ResponseEntity.ok(service.updateShareableStatus(bookId, connectedUser));
     }
+    //Update  archived status book
+    @PatchMapping("/archived/{book-id}")
+    public ResponseEntity<Integer> updateArchivedStatus(
+            @PathVariable("book-id") Integer bookId,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.updateArchivedStatus(bookId, connectedUser));
+    }
 
 }
--- book-network/src/main/java/com/frikha/book/book/BookService.java
+++ book-network/src/main/java/com/frikha/book/book/BookService.java
@@ -121,10 +121,22 @@ public class BookService {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
         User user = ((User) connectedUser.getPrincipal());
-        if (!Objects.equals(book.getOwner().getBooks(), user.getId())) {
+        if (!Objects.equals(book.getOwner().getId(), user.getId())) {
             throw new OperationNotPermittedException("You cannot update others books shareable status");
         }
         book.setShareable(!book.isShareable());
+        bookRepository.save(book);
+        return bookId;
+    }
+
+    public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+        User user = ((User) connectedUser.getPrincipal());
+        if (!Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot update others books archived status");
+        }
+        book.setArchived(!book.isArchived());
         bookRepository.save(book);
         return bookId;
     }
