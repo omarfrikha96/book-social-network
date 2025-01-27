4b089231c9901ea15f61d0d084aa07e70de83f03
omarfrikha96 <omar.frikha96@gmail.com>
2025-01-27 17:39:30 +0100

    find book by id

--- book-network/src/main/java/com/frikha/book/book/BookController.java
+++ book-network/src/main/java/com/frikha/book/book/BookController.java
@@ -5,10 +5,7 @@ import org.springframework.security.core.Authentication;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/books")
@@ -18,8 +15,16 @@ public class BookController {
 
     private final BookService service;
 
+    //Save book
     @PostMapping
     public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
         return ResponseEntity.ok(service.save(request, connectedUser));
     }
+
+    //Get book by id
+    @GetMapping("/{book-id}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
 }
