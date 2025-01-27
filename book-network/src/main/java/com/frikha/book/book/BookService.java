77a15491e9f40ad8e1aaec4b12daa636c7ad2fc5
omarfrikha96 <omar.frikha96@gmail.com>
2025-01-27 17:24:36 +0100

    add saveBook

---
+++ book-network/src/main/java/com/frikha/book/book/BookService.java
@@ -0,0 +1,30 @@
+package com.frikha.book.book;
+
+import com.frikha.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class BookService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public Integer save(BookRequest request, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return bookRepository.save(book).getId();
+    }
+}
