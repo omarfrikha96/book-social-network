02f1a193a4a1d6070c547e1de914cb41af64054d
omarfrikha96 <omar.frikha96@gmail.com>
2025-01-28 22:04:59 +0100

    update upload book cover picture

--- book-network/src/main/java/com/frikha/book/book/BookMapper.java
+++ book-network/src/main/java/com/frikha/book/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.frikha.book.book;
 
+import com.frikha.book.file.FileUtils;
 import com.frikha.book.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().getFullName())
-               // .cover()
+                .cover(FileUtils.readFileFromLocation(book.getBookCover())) // cover is a file path
                 .build();
     }
 
